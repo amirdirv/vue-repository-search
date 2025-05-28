01d6672f4d48264aeabeca95132947deb877c65e
amirmohammadirv <amir.mohammad@meyler.it>
2025-05-28 05:05:50 +0200

    RepoList component completed

--- src/components/RepoList.vue
+++ src/components/RepoList.vue
@@ -1,11 +1,28 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-
+  <div>
+    <div v-if="loading" class="has-text-centered mt-5">
+      <progress class="progress is-small is-primary" max="100">Loading</progress>
+    </div>
+    <div v-else>
+      <div
+          class="box"
+          v-for="repo in results"
+          :key="repo.id"
+      >
+        <strong>{{ repo.full_name }}</strong><br />
+        ⭐ {{ repo.stargazers_count }} | 🍴 {{ repo.forks_count }}
+        <p>{{ repo.description }}</p>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss">
+<script>
+import { mapState } from 'vuex'
 
-</style>
+export default {
+  computed: {
+    ...mapState(['results', 'loading'])
+  }
+}
+</script>
