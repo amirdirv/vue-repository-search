312b629d315f1eda1f53597c842e9105243d2d05
amirmohammadirv <amir.mohammad@meyler.it>
2025-05-28 05:06:36 +0200

    Pagination component completed

---
+++ src/components/PaginationControl.vue
@@ -0,0 +1,36 @@
+<template>
+  <nav class="pagination is-centered mt-4" role="navigation" aria-label="pagination">
+    <button class="button is-link" @click="prevPage" :disabled="page <= 1">Prev</button>
+    <button class="button is-link ml-2" @click="nextPage" :disabled="isLastPage">Next</button>
+  </nav>
+</template>
+
+<script>
+import { mapState, mapMutations, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['currentPage', 'total', 'perPage']),
+    page() {
+      return this.currentPage
+    },
+    isLastPage() {
+      return this.page * this.perPage >= this.total
+    }
+  },
+  methods: {
+    ...mapMutations(['SET_PAGE']),
+    ...mapActions(['searchRepos']),
+    nextPage() {
+      this.SET_PAGE(this.page + 1)
+      this.searchRepos()
+    },
+    prevPage() {
+      if (this.page > 1) {
+        this.SET_PAGE(this.page - 1)
+        this.searchRepos()
+      }
+    }
+  }
+}
+</script>
